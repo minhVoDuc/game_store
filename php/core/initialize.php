ee9450dc849f56a4dd944d999314136e1e21d9b2
minhVoDuc <vdm2312@gmail.com>
2023-04-05 19:34:11 +0700

    reconfig backend for hosting on centos

--- php/core/initialize.php
+++ php/core/initialize.php
@@ -1,6 +1,6 @@
 <?php
     defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-    defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'xampp'.DS.'htdocs'.DS.'assignment_hcmg');
+    defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'xampp'.DS.'htdocs'.DS.'game_store');
     //assignment_php/php/...
     defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'php'.DS.'includes');
     defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'php'.DS.'core');
